85f8b9c49850a43857549f0f822774202db7cab2
Lê Khai Trí <lekhaitri.2003@gmail.com>
2024-11-03 22:04:11 +0700

    hide navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,23 +1,33 @@
-import { useState } from 'react';
 import { FaHome, FaFolder } from 'react-icons/fa';
 
-export default function Navbar({ setView }) {
+export default function Navbar({ setView, isVisible }) {
   return (
-    <div className="w-[10%] h-screen bg-gray-800 text-white flex flex-col items-center justify-center py-4">
-      <button
-        onClick={() => setView('home')}
-        className="text-cyan-400 mb-6 flex flex-col items-center"
-      >
-        <FaHome className="mb-2" size={24} />
-        Home
-      </button>
-      <button
-        onClick={() => setView('viewTodo')}
-        className="text-cyan-400 flex flex-col items-center"
-      >
-        <FaFolder className="mb-2" size={24} />
-        View Todo
-      </button>
+    <div
+      className={`${
+        isVisible ? 'w-[10%]' : 'w-2'
+      } h-screen bg-gray-800 text-white flex flex-col items-center justify-center py-4 transition-all duration-300`}
+      style={{
+        borderRight: isVisible ? 'none' : '2px solid #2d3748', // Đường viền mỏng khi ẩn
+      }}
+    >
+      {isVisible && (
+        <>
+          <button
+            onClick={() => setView('home')}
+            className="text-cyan-400 mb-6 flex flex-col items-center"
+          >
+            <FaHome className="mb-2" size={24} />
+            Home
+          </button>
+          <button
+            onClick={() => setView('viewTodo')}
+            className="text-cyan-400 flex flex-col items-center"
+          >
+            <FaFolder className="mb-2" size={24} />
+            View Todo
+          </button>
+        </>
+      )}
     </div>
   );
 }
