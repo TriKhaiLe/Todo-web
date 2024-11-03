92616a9ce219b1f6437da0219887ce0db6b50092
Lê Khai Trí <lekhaitri.2003@gmail.com>
2024-11-03 21:52:14 +0700

    add icon navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { FaHome, FaList } from 'react-icons/fa';
+import { FaHome, FaFolder } from 'react-icons/fa';
 
 export default function Navbar({ setView }) {
   return (
-    <div className="w-[28.57%] h-screen bg-gray-800 text-white flex flex-col items-center py-4">
+    <div className="w-[10%] h-screen bg-gray-800 text-white flex flex-col items-center py-4">
       <button
         onClick={() => setView('home')}
         className="text-cyan-400 mb-6 flex flex-col items-center"
@@ -15,7 +15,7 @@ export default function Navbar({ setView }) {
         onClick={() => setView('viewTodo')}
         className="text-cyan-400 flex flex-col items-center"
       >
-        <FaList className="mb-2" size={24} />
+        <FaFolder className="mb-2" size={24} />
         View Todo
       </button>
     </div>
